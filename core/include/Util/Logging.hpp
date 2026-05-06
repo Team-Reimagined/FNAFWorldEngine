340b0b466ba4be8792f00e5345a2a09976054ef3
LimeGradient <fnafgamer1yt@gmail.com>
2026-05-05 20:15:33 -0400

    file logging

--- core/include/Util/Logging.hpp
+++ core/include/Util/Logging.hpp
@@ -2,7 +2,9 @@
 
 #include <chrono>
 #include <format>
+#include <fstream>
 #include <string>
+#include <sstream>
 #include <stdio.h>
 
 namespace FWE::Util {
@@ -11,24 +13,46 @@ namespace FWE::Util {
         template <typename... Args>
         static void info(std::format_string<Args...> fmt, Args&&... args) {
             std::string str = std::format(fmt, std::forward<Args>(args)...);
-            printf("[%s]: %s \x1b[0m \n", Logging::getCurrentTime().c_str(), str.c_str());
+            std::string log = std::format("[{}]: {} \x1b[0m \n", getCurrentTime(), str);
+            getLogFile() << std::format("[{}]: {}", getCurrentTime(), str) << std::endl;
+            printf(log.c_str());
         }
 
         template <class... Args>
         static void warn(std::format_string<Args...> fmt, Args&&... args) {
             std::string str = std::format(fmt, std::forward<Args>(args)...);
-            printf("\e[0;33m[%s]: %s \x1b[0m \n", Logging::getCurrentTime().c_str(), str.c_str());
+            std::string log = std::format("\e[0;33m[{}]: {} \x1b[0m \n", getCurrentTime(), str);
+            getLogFile() << std::format("[{}]: {}", getCurrentTime(), str) << std::endl;
+            printf(log.c_str());
         }
 
         template <class... Args>
         static void error(std::format_string<Args...> fmt, Args&&... args) {
             std::string str = std::format(fmt, std::forward<Args>(args)...);
-            printf("\e[0;31m[%s]: %s \x1b[0m \n", Logging::getCurrentTime().c_str(), str.c_str());
+            std::string log = std::format("\e[0;31m[{}]: {} \x1b[0m \n", getCurrentTime(), str);
+            getLogFile() << std::format("[{}]: {}", getCurrentTime(), str) << std::endl;
+            printf(log.c_str());
         }
 
     private:
         static std::string getCurrentTime() {
             return std::format("{:%T}", std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now())).c_str();
+        }
+
+        static std::ofstream& getLogFile() {
+            auto t = std::time(nullptr);
+            auto tm = *std::localtime(&t);
+            std::ostringstream time;
+            time << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+
+            std::string filename = std::format("logs/log-{}.txt", time.str());
+            static std::ofstream logFile(filename);
+            if (!logFile.is_open()) {
+                logFile.open(filename, std::fstream::out | std::fstream::trunc);
+                logFile <<"\n";
+                logFile.close();
+            }
+            return logFile;
         }
     };
 }
--- core/test/main.cpp
+++ core/test/main.cpp
@@ -1,4 +1,5 @@
 #include "Renderer/Renderer.hpp"
+#include "Audio/AudioManager.hpp"
 #include <thread>
 #include "Audio/AudioManager.hpp"
 #include "backends/imgui_impl_sdl3.h"
